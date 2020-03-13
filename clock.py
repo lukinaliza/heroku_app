4673ef8ecd8abff0ff5a4250b9fbefc07d34fade
OneEarthling <48918578+OneEarthling@users.noreply.github.com>
2020-03-13 11:55:09 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -21,7 +21,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', minutes=5)
 def timed_job():
     session = Session()
     users = session.query(User)
